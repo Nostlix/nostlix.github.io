af1e3e44569433992f2435becef0918a19d213d4
Valentin DUNON <valentin.dunon@epitech.eu>
2025-12-23 14:53:11 +0100

    feat: improved image placement

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -7,38 +7,36 @@ export default function Hero()
 {
     return (
         <header className="hero">
-            <div className="h-100 flex flex-row hero-cols">
+            <div className="h-1/1 flex flex-row hero-cols">
                 <div className="flex flex-col justify-center">
-                    <div className="py-10">
+                    <div className="">
                         <h1 className="title">Valentin DUNON</h1>
                         <h2 className="subtitle">Epitech Student</h2>
                     </div>
-                    <h3>
+                    <h3 className="py-10">
                         A future expert involved inside new technologies industry.
                     </h3>
-                </div>
-                <div>
-                    <div className="absolute -ml-45 w-160 h-100">
-                        <Image
-                            className=""
-                            src="/image_hero.png"
-                            alt="A man sitting near a desk"
-                            width={935}
-                            height={853}
-                            sizes="(max-width: 768px) 40vw, 33vw"
-                            priority
-                        />
+                    <div className="flex gap-5 py-4">
+                        <Button variant="primary">Discover Me</Button>
+                        <Button variant="tertiary">Upload CV</Button>
+                    </div>
+                    <div className="flex gap-5">
+                        <Social>Github</Social>
+                        <Social>Github</Social>
+                        <Social>Github</Social>
                     </div>
                 </div>
-            </div>
-            <div className="flex gap-5 pb-4">
-                <Button variant="primary">Discover Me</Button>
-                <Button variant="tertiary">Upload CV</Button>
-            </div>
-            <div className="flex gap-5">
-                <Social>Github</Social>
-                <Social>Github</Social>
-                <Social>Github</Social>
+                <div className="flex content-center">
+                    <Image
+                        className="-ml-5 max-h-6/10 self-center"
+                        src="/image_hero.png"
+                        alt="A man sitting near a desk"
+                        width={935}
+                        height={853}
+                        sizes="100%"
+                        priority
+                    />
+                </div>
             </div>
         </header>
     )
